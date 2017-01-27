4f177564b906eb5cf05e97831f7abb8ad35bdb64
Paul Buxton <paul.buxton@kl.imgtec.org>
2017-01-27 13:10:11 +0000

    Initial work untested

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # myha
 Simple home automation scripting using various libraries
+
+
+Depends on limitlesled
+pip install git+https://github.com/soldag/python-limitlessled.git
+
+and Pika
+pip install pika
+
+The receiver will listen for messages and send them on to the limitlessled bridge.
+Messages are of the form
+either
+lightgroup : action <value>
+trigger :
+Where action can be
+    on, off, brightness, temperature, colour (depending on the colour group)
+
+
+The light groups will simply pass on the commands as provided
+The trigger will allow further processing e.g. 
+trigger : doorswitch
+Would invoke a customer command in the receiver which will turn on a light and then after a number of seconds turn it off. In order to prevent the light toggling off due to multiple triggers
+easiest way will be to use a semaphore.
+E.g.
+
+trigger -> turn on light, increment semaphore, set timer
+timer -> decrement semaphore, if 0 turn off light.
+
+Semaphore would be attached to the thing being contolled rather than trigger
+
---
+++ lightserver.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import pika
+import time, threading
+
+from limitlessled.bridge import Bridge
+from limitlessled.group.rgbw import RGBW
+from limitlessled.group.white import WHITE
+
+bridge = Bridge('192.168.0.34')
+bedroom  =bridge.add_group(1, 'bedroom', RGBW)
+# A group number can support two groups as long as the types differ
+nightlight = bridge.add_group(1, 'nightlight', BRIDGE_LED)
+hall = bridge.add_group(2, 'hall', RGBW)
+spare_room = bridge.add_group(2, 'spare_room', WHITE)
+
+
+# create a single lock that is used by all threads
+hall_lock = threading.Lock()
+hall_counter = 0
+
+
+def timehandler(locker, counter, group):
+    with locker:
+        if counter == 0:
+            print "Error, attempt to decrement zero counter"
+        else:
+            counter = counter -1
+            if counter == 0:
+                # this is the last event outstanding so we can turn the group off
+                group.off = True
+
+
+
+
+
+def rabbitcallback(ch, method, properties, body):
+    print " [x] Received %r" % body
+    # parse the command
+    params = body.split()
+    group = params[0]
+    if group in bridge.groups:
+        # we have a valid group
+        targetgroup = bridge.groups[group]
+        if params[1] == 'on':
+            targetgroup.on = True
+        elif params[1] == 'off':
+            targetgroup.off = True
+        else:
+            # we assume this is probably a brightness call
+            if params[1] == 'brightness':
+                targetgroup.brightness = params[2]/100.0 #incoming brighness is done as a percentage
+    elif (params[0] == 'door') | (params[1] == 'pir'):
+        with hall_lock:
+            hall.on = True
+            # set a timer to go off after 15 seconds
+            threading.Timer(15.0, timehandler, hall_lock, hall_counter, hall).start()
+    else:
+        print 'Unknown command {}'.format(params[0])
+
+
+
+
+
+
+
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='lightserver')
+
+
+channel.basic_consume(rabbitcallback,
+                      queue='lightserver',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
+
+
+
+
+
