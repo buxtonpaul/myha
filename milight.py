f67e6fc0316c100562129e6137767a7882d661fc
Paul Buxton <Paul Buxton>
2017-01-31 13:57:57 +0000

    Updates to make lights objects

--- milight.py
+++ milight.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import socket
-import mylightcmds
+from milightcmds import *
 
 
 
@@ -43,9 +43,10 @@ class milight_bridge(object):
     live = True
     SessionID1 = 0
     SessionID2 = 0
-    sockserver = 0
+    sockServer = 0
     iboxip = ""
     cyclenr = 0
+    bridgeopen=False
     ''' Miligght bridge class to handle controlling a milight bridge'''
     def __init__(self, IBOX_IP="fake",        # iBox IP address
                  UDP_PORT_SEND=5987,            # Port for sending
@@ -61,18 +62,21 @@ class milight_bridge(object):
         self.iboxip = IBOX_IP
         self.udp_port_send = UDP_PORT_SEND
         if IBOX_IP == 'fake':
-            live = False
+            self.live = False
             dolog("Starting up with fake settings")
         dolog("Trying to connect to server {} on port {}".format(self.iboxip, UDP_PORT_SEND))
         for icount in range(0, UDP_MAX_TRY):
             try:
-                if live:
+                if self.live:
                     self.sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                     self.sockServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                     self.sockServer.bind(('', UDP_PORT_RECEIVE))
                     self.sockServer.settimeout(UDP_TIMEOUT)
-                    self.sockServer.sendto(START_SESSION, (self.iboxip,  self.udp_port_send))
+                    self.sockServer.sendto( bytearray(START_SESSION), (self.iboxip,  self.udp_port_send))
+            
+                   
                     dataReceived, addr = self.sockServer.recvfrom(1024)
+                    dataReceived = bytearray(dataReceived)
                 else:
                     dolog("Fake it")
                     dataReceived = [0x28, 0x00, 0x00, 0x00, 0x11, 0x00, 0x02, 0xAC, 0xCF,
@@ -86,30 +90,33 @@ class milight_bridge(object):
                 self.lightSession = True
                 break
             except socket.timeout:
-                    print "Timeout on session start: ", START_SESSION
+                    print "Timeout on session start: ", hexstr(START_SESSION)
                     dolog("Milight Script: Timeout on command... doing a retry")
                     self.sockServer.close()
+                    self.lightSession=False
                     continue
 
     def sndCommand(self,message):
         ''' Send a list of hex values as a message '''
-        if self.live:
-            self.sockServer.sendto(bytearray.fromhex(message), (self.iboxip, self.udp_port_send))
+        if self.live & self.lightSession:
+            self.sockServer.sendto(bytearray(message), (self.iboxip, self.udp_port_send))
             dataReceived, addr = self.sockServer.recvfrom(1024)
-            dolog("Receiving response: {response}".format(response=hexstr(dataReceived, ' ')))
+            dolog("Receiving response: {response}".format(response=hexstr(bytearray(dataReceived), ' ')))
             return(dataReceived,addr)
         else:
             return([0x1,0x2,0x3],1234)
             
-    def buildCmd(self,cyclenr, bulbCommand, Zone, checkSum):
+    def buildCmd(self, bulbCommand, Zone, checkSum):
         preamble = [0x80, 0x00, 0x00, 0x00, 0x11]
         ret=(preamble + [self.SessionID1, self.SessionID2, 0x00, self.cyclenr, 0x00] +
               bulbCommand + [Zone, 0x00, checkSum])
         self.cyclenr = self.cyclenr + 1
         return ret
 
-    def __del__(self):
-        self.sockServer.close()
+    def close(self):
+        if(self.live & self.lightSession):
+            print "closing socket"
+            self.sockServer.close()
 
 
 
@@ -123,35 +130,112 @@ def hexstr(vals, sep=', '):
     return'[{}]'.format(sep.join(hex(x) for x in vals))
 
 
+from abc import ABCMeta, abstractmethod
 
+class MiLight(object):
+    ''' An object for the milight abstract milight 
+    Attributes:
+    name: name of the light represented by this case
+    zone: the zone that this light is attached to
+    bridge: the bridge this light is attached to
+    '''
+    __metaclass__ = ABCMeta
 
+    def __init__(self,name,zone,bridge):
+        self.bridge=bridge
+        self.name=name
+        self.zone=zone
 
+    def on(self):
+        # construct the command and send it
+        self.doCommand("ON")
+    def off(self):
+        self.doCommand("OFF")
+        # construct the command and send it
 
-DEVICES = {
-    "BRIDGE" : [BRIDGECOMMANDS, BRIDGEVARCOMMANDS],
-    "RGBW" : [RGBWCOMMANDS, RGBWVARCOMMANDS],
-    "RAWCOMMANDS" : [RAWCOMMANDS],
-    "WHITE" : [WHITECOMMANDS],
+    def doCommand(self,command,value='none'):
+        if command in self.commands:
+            commandstring=self.commands.get(command)
+            Checksum = sum(commandstring) & 0xff
+            sendCommand = self.bridge.buildCmd(commandstring, self.zone, Checksum)
+            dolog("Sending Command:{}".format(hexstr(bytearray(sendCommand))))
+            dataReceived, addr = self.bridge.sndCommand(bytearray(sendCommand))
+            dolog("Received Reponse:{}".format(hexstr(bytearray(dataReceived))))
+        elif command in self.varcommands:
+            if value!='none':
+                commandstring=self.varcommands.get(command)+[value] + [0x00, 0x00, 0x00]
+                Checksum = sum(commandstring) & 0xff
+                sendCommand = self.bridge.buildCmd(commandstring, self.zone, Checksum)
+                dolog("Sending Command:{}".format(hexstr(bytearray(sendCommand))))
+                dataReceived, addr = self.bridge.sndCommand(bytearray(sendCommand))
+                dolog("Received Reponse:{}".format(hexstr(bytearray(dataReceived))))
+            else:
+                dolog("Request to do variable command with no param provided")
+        else:
+            dolog("Command {} not found".format(command))
+ 
+
+class BridgeLight(MiLight):
+    ''' The Bridge Light object'''
+    commands={
+        "ON":    [0x31, 0x00, 0x00, 0x00, 0x03, 0x03, 0x00, 0x00, 0x00],
+        "OFF":   [0x31, 0x00, 0x00, 0x00, 0x03, 0x04, 0x00, 0x00, 0x00],
+        "WHITE": [0x31, 0x00, 0x00, 0x00, 0x03, 0x05, 0x00, 0x00, 0x00]
     }
+    varcommands={
+    "BRIGHT"   :  [0x31, 0x00, 0x00, 0x00, 0x02],
+    "MODE"     :  [0x31, 0x00, 0x00, 0x00, 0x04]
+    }
+
+
+class RGBW(MiLight):
+    ''' The RGBW Light object'''
+    commands={
+    "ON" :   [0x31, 0x00, 0x00, 0x07, 0x03, 0x01, 0x00, 0x00, 0x00],
+    "OFF":   [0x31, 0x00, 0x00, 0x07, 0x03, 0x02, 0x00, 0x00, 0x00],
+    "NIGHT": [0x31, 0x00, 0x00, 0x07, 0x03, 0x06, 0x00, 0x00, 0x00],
+    "WHITE": [0x31, 0x00, 0x00, 0x07, 0x03, 0x05, 0x00, 0x00, 0x00]
+    
+    }
+    varcommands={
+    "BRIGHT"   : [0x31, 0x00, 0x00, 0x07, 0x02],
+    "MODE"     : [0x31, 0x00, 0x00, 0x07, 0x04]}
+
+class White(MiLight):
+    commands = {
+    "ON"        : [0x31, 0x00, 0x00, 0x01, 0x01, 0x07, 0x00, 0x00, 0x00],
+    "OFF"       : [0x31, 0x00, 0x00, 0x01, 0x01, 0x08, 0x00, 0x00, 0x00],
+    "NIGHT"     : [0x31, 0x00, 0x00, 0x01, 0x01, 0x06, 0x00, 0x00, 0x00],
+    "BRIGHTUP"  : [0x31, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00],
+    "BRIGHTDOWN": [0x31, 0x00, 0x00, 0x01, 0x01, 0x02, 0x00, 0x00, 0x00],
+    "TEMPUP"    : [0x31, 0x00, 0x00, 0x01, 0x01, 0x03, 0x00, 0x00, 0x00],
+    "TEMPDOWN"  : [0x31, 0x00, 0x00, 0x01, 0x01, 0x04, 0x00, 0x00, 0x00],
+    
+    }
+    varcommands={}
+
 
 ######################
 ## iBox v6 commands ##
 ######################
 
-def iBoxV6Commands(device, cmd, value):
-    ''' Construct a MiLightBox command from the device command and value '''
-    if cmd in DEVICES[device][0]:
-        return DEVICES[device][0].get(cmd)
-    if device == 'RAWCOMMANDS':
-        dolog('Command not found:{cmd}'.format(cmd=cmd))
-        return 0
-    if cmd in DEVICES[device][1]:
-        dolog("Variable command: {cmd} {value}".format(cmd=cmd, value=value))
-        retval = DEVICES[device][1].get(cmd)+ [value] + [0x00, 0x00, 0x00]
-        dolog("Trying: {command}".format(command=hexstr(retval)))
-        return retval
-    dolog('Command not found')
-    return 0
+#def iBoxV6Commands(device, cmd, value):
+#    ''' Construct a MiLightBox command from the device command and value '''
+#    if cmd in DEVICES[device][0]:
+#        return DEVICES[device][0].get(cmd)
+#    if device == 'RAWCOMMANDS':
+#        dolog('Command not found:{cmd}'.format(cmd=cmd))
+#        return 0
+#    if cmd in DEVICES[device][1]:
+#        dolog("Variable command: {cmd} {value}".format(cmd=cmd, value=value))
+#        retval = DEVICES[device][1].get(cmd)+ [value] + [0x00, 0x00, 0x00]
+#        dolog("Trying: {command}".format(command=hexstr(retval)))
+#        return retval
+#    dolog('Command not found')
+#    return 0
+
+
+
 
 
 
@@ -161,62 +245,30 @@ def iBoxV6Commands(device, cmd, value):
 ########################
 if __name__ == "__main__":
     import sys
-    ##########################
-    ## Commandline commands ##
-    ##########################
+    CMDLINE_CMD = 'ON'
+    CMDLINE_ZONE = 0
+    CMDLINE_VALUE1 = 100
+    CMDLINE_DEVICE = "RGBW"
     
-    try:
-        CMDLINE_DEVICE = sys.argv[1].strip()
-        dolog("Target: {target}".format(target=CMDLINE_DEVICE))
-        CMDLINE_ZONE = int(sys.argv[2].strip())
-        dolog("ZONE: {zone}".format(zone=CMDLINE_ZONE))
+    maxtries=5
+    
+    
+    bridge = milight_bridge(UDP_MAX_TRY=maxtries)
+  #  bridge = milight_bridge(IBOX_IP="192.168.0.34", UDP_MAX_TRY=maxtries)
 
-        CMDLINE_CMD = sys.argv[3].strip()
-        dolog("CMD : {cmd}".format(cmd=CMDLINE_CMD))
-        CMDLINE_VALUE1 = 0
-        ## check the device exists
-        if CMDLINE_DEVICE in DEVICES:
-            dolog("Using device: {device}".format(device=CMDLINE_DEVICE))
-        else:
-            dolog("No device found matching: {device}".format(device=CMDLINE_DEVICE))
-            raise SystemExit()
-        if CMDLINE_CMD in DEVICES[CMDLINE_DEVICE][0]:
-            dolog("Static command found: {cmd}".format(cmd=CMDLINE_CMD))
-        elif CMDLINE_CMD in DEVICES[CMDLINE_DEVICE][1]:
-            dolog("Checking variable command {cmd}".format(cmd=CMDLINE_CMD))
-            CMDLINE_VALUE1 = int(sys.argv[4].strip())
-            dolog("Variable command found: {cmd} {var}".format(cmd=CMDLINE_CMD, var=CMDLINE_VALUE1))
-        else:
-            raise SystemExit()
-    except:
-        dolog("Valid command not found")
-        print CMDLINE_INFO
-        raise SystemExit()
-    
-    bridge = milight_bridge()
-    for icount in range(0, UDP_MAX_TRY):
+    mylight=BridgeLight("mylight",0,bridge)
+
+    for icount in range(0, maxtries):
         try:
-            dolog("Cycle number: {cycle}".format(cycle=cyclenr))
-            bulbCommand = iBoxV6Commands(CMDLINE_DEVICE, CMDLINE_CMD, CMDLINE_VALUE1)
-            dolog("Light command: {Command}".format(Command=hexstr(bulbCommand, ' ')))
-            useZone = CMDLINE_ZONE
-            dolog("Zone: {Zone}".format(Zone=useZone))
-
-            Checksum = sum(bulbCommand) & 0xff
-            dolog("Checksum: {checksum}".format(checksum=Checksum))
-
-            sendCommand = bridge.buildCmd(bulbCommand, useZone, Checksum)
-
-            dolog("Sending command: {command}".format(command=hexstr(sendCommand, ' ')))
-
-            dataReceived, addr = bridge.sndCommand(bytearray.fromhex(sendCommand))
-            dolog("Receiving response: {}".format( hexstr(dataReceived, ' ')) )
+            mylight.off()
+            mylight.doCommand('BRIGHT',51)
+            
             break
 
         except socket.timeout:
             dolog("Timeout on command: {command}".format(command=hexstr(sendCommand, ' ')))
             continue
-
+    bridge.close()
     
 
 #"192.168.0.34"
